d39963ab0ef1eb883e0b8e753a7aaff28a98d80f
de-yu <symbian3meego@Gmail.com>
2018-08-09 21:50:09 +0800

    note

--- todolist/src/js/Hello.js
+++ todolist/src/js/Hello.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Hello extends React.Component {
   render() {
-    return <h1>Hello, webpack!</h1>;
+    return <h1>Hell, wepack!</h1>;
   }
 }
 
--- todolist/webpack.config.js
+++ todolist/webpack.config.js
@@ -1,30 +1,31 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var DIST_DIR = path.resolve(__dirname,'dist');
-var SRC_DIR = path.resolve(__dirname,"src");
+var DIST_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, "src");
 
 
 var config = {
-  entry:[SRC_DIR + "/js/index.js"],
-  output:{
-    path:DIST_DIR + "/js/",
-    filename:"bundle.js"
-  },
-  devServer:{
-    contentBase:path.join(__dirname,"dist"),
-    port:8080
-  },
-   module: {
-        loaders: [
-          {
-              test: /\.jsx?$/,
-              exclude: /node_modules/,
-              loader: "babel-loader",
-              query: {
-                presets:['es2015', 'react']
-              }
-          }
+    entry: SRC_DIR + "/js/index.js",
+    output: {
+        path: DIST_DIR + "/js/",
+        filename: "bundle.js",
+        publicPath: "/js/"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?/, exclude: /node_modules/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
         ]
     }
 }
